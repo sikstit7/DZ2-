a27b25bd94de72b9085e2ec8c034c55117ab912c
Пользователь <kari.inago@icloud.com>
2019-11-06 08:34:07 +0300

    попытка 2

---
+++ ДЗ(2)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ДЗ_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double resyltat;
+            char oper;
+            Console.WriteLine("введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+            if (oper == '+') 
+            {
+                resyltat = a + b;
+                Console.WriteLine("Сумма равна:" + resyltat);
+            }   else if (oper == '-')
+            {
+                resyltat = a - b;
+                Console.WriteLine("Разность равна:" + resyltat);
+            }   else if (oper == '*')
+            {
+                resyltat = a * b;
+                Console.WriteLine("Производная равно:" + resyltat);
+            }   else if (oper == '/')
+            {
+                resyltat = a / b;
+                Console.WriteLine("Деление равно:" + resyltat);
+            }   else
+            {
+                Console.WriteLine("Неизвестный оператор");
+            }
+            Console.ReadKey();
+        }
+    }
+}
